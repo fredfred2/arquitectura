d15d3d7a187041f8d2085889db72f4cb231dd93f
fredfred2@gmail.com <fredfred2@gmail.com>
2017-06-17 11:41:24 -0500

    Se sube solucion de leccion 5

---
+++ jpa/solutions/05_modeling/AuctionApplicationL5P2/src/java/com/example/model/AuctionUser.java
@@ -0,0 +1,79 @@
+package com.example.model;
+
+import java.io.Serializable;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class AuctionUser implements Serializable {
+
+    @Id
+    @Column(name="USERID")
+    private String displayName;
+    private String password;
+    private String email;
+    private String street;
+    private String cityState;
+    private String zip;
+
+    public AuctionUser() {
+    }
+
+    public AuctionUser(String displayName, String password, String email, String street, String cityState, String zip) {
+        this.displayName = displayName;
+        this.password = password;
+        this.email = email;
+        this.street = street;
+        this.cityState = cityState;
+        this.zip = zip;
+    }
+
+    public String getDisplayName() {
+        return displayName;
+    }
+
+    public void setDisplayName(String displayName) {
+        this.displayName = displayName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public String getCityState() {
+        return cityState;
+    }
+
+    public void setCityState(String cityState) {
+        this.cityState = cityState;
+    }
+
+    public String getZip() {
+        return zip;
+    }
+
+    public void setZip(String zip) {
+        this.zip = zip;
+    }
+}
---
+++ jpa/solutions/05_modeling/AuctionApplicationL5P4/src/java/com/example/model/Item.java
@@ -0,0 +1,76 @@
+package com.example.model;
+
+import java.io.Serializable;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
+
+@Entity
+public class Item implements Serializable {
+
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id
+    private int itemId;
+    @OneToOne
+    @JoinColumn(name = "IMAGEID")
+    private Image image;
+    private String title;
+    private String description;
+    @Column(name = "ITEMCONDITION")
+    private String condition;  // NEW, USED, PARTS
+
+    public Item() {
+    }
+
+    public Item(int itemId, Image image, String title, String description, String condition) {
+        this.itemId = itemId;
+        this.image = image;
+        this.title = title;
+        this.description = description;
+        this.condition = condition;
+    }
+
+    public int getItemId() {
+        return itemId;
+    }
+
+    public void setItemId(int itemId) {
+        this.itemId = itemId;
+    }
+
+    public Image getImage() {
+        return image;
+    }
+
+    public void setImage(Image image) {
+        this.image = image;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getCondition() {
+        return condition;
+    }
+
+    public void setCondition(String condition) {
+        this.condition = condition;
+    }
+}

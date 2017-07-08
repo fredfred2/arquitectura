bec8a9de94b1aeb6fc53f974cd13241f3c4e8119
fredfred2@gmail.com <fredfred2@gmail.com>
2017-07-08 11:30:29 -0500

    Se suben soluciones

---
+++ jpa/solutions/11_locking/AuctionApplicationL11P1/src/java/com/example/util/AuctionEndedException.java
@@ -0,0 +1,20 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.example.util;
+
+/**
+ *
+ * @author tmcginn
+ */
+public class AuctionEndedException extends Exception {
+
+    public AuctionEndedException() {
+    }
+
+    public AuctionEndedException(String message) {
+        super(message);
+    }
+    
+}

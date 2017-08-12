b5ad679a5a698c36a9d0323f444231b65841cdb0
Raul Marquez <RAUL.MARQUEZ@intellego.com.mx>
2017-08-12 13:18:49 -0500

    solution 6

---
+++ jsf/06_MsgBundles/solution/solution1/DVDLibrary_L6P1/src/java/com/example/exceptions/PreexistingEntityException.java
@@ -0,0 +1,16 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.example.exceptions;
+
+/**
+ *
+ * @author parodutt
+ */
+public class PreexistingEntityException extends Exception {
+     public PreexistingEntityException(String message) {
+        
+     super(message);
+    }
+}

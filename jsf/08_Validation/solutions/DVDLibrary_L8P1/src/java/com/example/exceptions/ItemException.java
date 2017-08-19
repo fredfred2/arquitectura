215d1c6baa2e1276faa5dd338d6314bd8b955efb
Raul Marquez <RAUL.MARQUEZ@intellego.com.mx>
2017-08-19 11:58:18 -0500

    example 8

---
+++ jsf/08_Validation/solutions/DVDLibrary_L8P1/src/java/com/example/exceptions/ItemException.java
@@ -0,0 +1,20 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.example.exceptions;
+
+/**
+ *
+ * @author parodutt
+ */
+public class ItemException extends Exception {
+
+    public ItemException(String message) {
+        
+       
+        super(message);
+    }
+    }
+    
+
---
+++ jsf/08_Validation/solutions/DVDLibrary_L8P4/src/java/com/example/entities/Item.java
@@ -0,0 +1,126 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.example.entities;
+
+import java.io.Serializable;
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import javax.xml.bind.annotation.XmlRootElement;
+
+/**
+ *
+ * @author parodutt
+ */
+@Entity
+@Table(name = "ITEM")
+@XmlRootElement
+@NamedQueries({
+    @NamedQuery(name = "Item.findAll", query = "SELECT i FROM Item i"),
+    @NamedQuery(name = "Item.findById", query = "SELECT i FROM Item i WHERE i.id = :id"),
+    @NamedQuery(name = "Item.findByTitle", query = "SELECT i FROM Item i WHERE i.title = :title"),
+    @NamedQuery(name = "Item.findByReleaseyear", query = "SELECT i FROM Item i WHERE i.releaseyear = :releaseyear"),
+    @NamedQuery(name = "Item.findByGenre", query = "SELECT i FROM Item i WHERE i.genre = :genre")})
+public class Item implements Serializable {
+    private static final long serialVersionUID = 1L;
+    @Id
+    @Basic(optional = false)
+    @NotNull
+    @Column(name = "ID")
+    private Integer id;
+    @Basic(optional = false)
+    @NotNull
+    @Size(min = 1, max = 100)
+    @Column(name = "TITLE")
+    private String title;
+    @Basic(optional = false)
+    @NotNull
+    @Size(min = 1, max = 4)
+    @Column(name = "RELEASEYEAR")
+    private String releaseyear;
+    @Basic(optional = false)
+    @NotNull
+    @Size(min = 1, max = 20)
+    @Column(name = "GENRE")
+    private String genre;
+
+    public Item() {
+    }
+
+    public Item(Integer id) {
+        this.id = id;
+    }
+
+    public Item(Integer id, String title, String releaseyear, String genre) {
+        this.id = id;
+        this.title = title;
+        this.releaseyear = releaseyear;
+        this.genre = genre;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getReleaseyear() {
+        return releaseyear;
+    }
+
+    public void setReleaseyear(String releaseyear) {
+        this.releaseyear = releaseyear;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Item)) {
+            return false;
+        }
+        Item other = (Item) object;
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "com.example.entities.Item[ id=" + id + " ]";
+    }
+    
+}

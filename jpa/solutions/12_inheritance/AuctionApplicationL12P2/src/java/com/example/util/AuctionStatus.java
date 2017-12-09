896124fbc0dee6f497c5a3c4877d107dab0019f0
Papu <fredfred2@gmail.com>
2017-12-09 13:57:35 -0600

    D

---
+++ jpa/solutions/12_inheritance/AuctionApplicationL12P2/src/java/com/example/util/AuctionStatus.java
@@ -0,0 +1,15 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.example.util;
+
+/**
+ *
+ * @author tmcginn
+ */
+public enum AuctionStatus {
+
+    ACTIVE, ENDED, CANCELLED
+
+}

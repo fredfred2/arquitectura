d407dd1a275a906e6e48dbc303132d03242d9462
Papu <fredfred2@gmail.com.mx>
2018-04-28 08:21:35 -0500

    reset

--- jpa/resources/scripts/AddTags.sql
+++
@@ -1,9 +0,0 @@
-# Tags table addition
-#
-
-DROP TABLE Tags;
-
-CREATE TABLE Tags (
-    ItemID INTEGER NOT NULL,
-    Keywords VARCHAR(20) NOT NULL
-);
--- jsf/14_Security/solutions/DVDLibrary_L14P1/build/web/WEB-INF/classes/com/example/beans/TableData.rs
+++
@@ -1,4 +0,0 @@
-com.test.beans.TableData
-com.test.beans.TableData$2
-com.test.beans.TableData$3
-com.test.beans.TableData$1
